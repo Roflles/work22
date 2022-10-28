e800dd5c198eb673fc742d72c92973f0eed7652c
Roflles <igxfrunza@gmail.com>
2022-10-28 15:44:13 +0200

    28.10

---
+++ dom/dom.js
@@ -0,0 +1,14 @@
+function init() {
+    var planet = document.getElementById("greenplanet");
+    planet.innerHTML = "Red Alert: hit by phaser fire!";
+    planet.setAttribute("class", "redtext");
+
+    var song1 = document.getElementById("song1");
+    var song2 = document.getElementById("song2");
+    var song3 = document.getElementById("song3");
+
+    song1.innerHTML = "Blue Suede Strings, by Elvis Pagely";
+    song2.innerHTML = "Great Objects on Fire, by Jerry JSON Lewis";
+    song3.innerHTML = "I Code the Line, by Johnny JavaScript";
+}
+window.onload = init;
